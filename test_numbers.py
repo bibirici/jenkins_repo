925b5f70fb2c54112191e32daf232b00ea45c568
abibiric <abibiric@A.BIBIRICI-M-M6RC>
2024-04-22 19:35:20 +0300

    modified test_numbers

--- test_numbers.py
+++ test_numbers.py
@@ -2,5 +2,5 @@ def test_int(parse_toml_with_numbers):
     numbers_toml = parse_toml_with_numbers
     print(f'number1: {numbers_toml["parameters"]["number1"]}')
     print(f'number2: {numbers_toml["parameters"]["number2"]}')
-    assert isinstance(numbers_toml["parameters"]["number1"], str)
-    assert isinstance(numbers_toml["parameters"]["number2"], str)
+    assert isinstance(numbers_toml["parameters"]["number1"], int)
+    assert isinstance(numbers_toml["parameters"]["number2"], int)
